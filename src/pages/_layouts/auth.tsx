dc0012be03fbaa46fdc74f16694124c48eac2ba1
Evandro Calado <evandrocalado07@gmail.com>
2025-03-05 18:46:59 -0300

    feat: add Radix UI label component and input component, update SignIn and AuthLayout for improved UI

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,24 @@
-import { Outlet } from 'react-router';
+import { Pizza } from 'lucide-react';
+import { Link, Outlet } from 'react-router';
 
 export const AuthLayout = () => {
   return (
-    <main>
-      <header>Header</header>
+    <main className='grid min-h-screen md:grid-cols-2'>
+      <div className='border-foreground/5 bg-muted text-muted-foreground hidden h-full flex-col justify-between border-r p-10 md:flex'>
+        <Link
+          to='/'
+          className='text-foreground flex items-center gap-3 text-lg font-medium'
+        >
+          <Pizza className='size-5' />
+          <span>Pizza Shop</span>
+        </Link>
 
-      <div>
+        <footer className='text-sm'>
+          Painel do parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className='flex flex-col items-center justify-center'>
         <Outlet />
       </div>
     </main>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,37 @@
 import { Helmet } from 'react-helmet-async';
 
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
 export const SignIn = () => {
   return (
     <>
       <Helmet title='Sign In' />
 
-      <h1>SignIn</h1>
+      <div className='p-5'>
+        <div className='flex w-[320px] flex-col justify-center gap-6'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Acessar painel
+            </h1>
+            <p className='text-muted-foreground text-sm'>
+              Acompanhe as vendas de sua loja
+            </p>
+          </div>
+
+          <form className='space-y-8'>
+            <div className='space-y-2'>
+              <Label htmlFor='email' className='text-sm'>
+                Email
+              </Label>
+              <Input id='email' type='email' />
+            </div>
+
+            <Button className='w-full cursor-pointer'>Acessar painel</Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
