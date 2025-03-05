b8c9242d5fa17fb255b1421da11a0388dd7778e9
Evandro Calado <evandrocalado07@gmail.com>
2025-03-05 20:58:46 -0300

    feat: add react-hook-form and zod for form validation in SignIn page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,34 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoaderCircle } from 'lucide-react';
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
+
 export const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    resolver: zodResolver(signInForm),
+  });
+
+  const handleSubmitForm = async (data: SignInForm) => {
+    console.log(data);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+
   return (
     <>
       <Helmet title='Sign In' />
@@ -20,15 +44,23 @@ export const SignIn = () => {
             </p>
           </div>
 
-          <form className='space-y-8'>
+          <form className='space-y-8' onSubmit={handleSubmit(handleSubmitForm)}>
             <div className='space-y-2'>
               <Label htmlFor='email' className='text-sm'>
                 Email
               </Label>
-              <Input id='email' type='email' />
+              <Input id='email' type='email' {...register('email')} />
             </div>
 
-            <Button className='w-full cursor-pointer'>Acessar painel</Button>
+            <Button className='w-full cursor-pointer' disabled={isSubmitting}>
+              {isSubmitting ? (
+                <div className='flex items-center gap-2'>
+                  Acessando... <LoaderCircle className='animate-spin' />
+                </div>
+              ) : (
+                'Acessar painel'
+              )}
+            </Button>
           </form>
         </div>
       </div>
