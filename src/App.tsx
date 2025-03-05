058198d59ade63b78729b245104be6ac1ed0f149
Evandro Calado <evandrocalado07@gmail.com>
2025-03-05 17:32:17 -0300

    feat: configure Prettier and ESLint, update styles, and enhance App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Button } from '@/components/ui/button';
 
 export const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className='p-5'>
+      <h1 className='flex text-3xl underline'>App</h1>
 
       <Button>Text</Button>
     </div>
