7731f9eb675705a3a9fabab42bef51ad6a2e09cd
Evandro Calado <evandrocalado07@gmail.com>
2025-03-10 17:45:32 -0300

    feat: add response interceptor to handle 401 errors and redirect to sign-in

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,29 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+
+import { isAxiosError } from 'axios';
+import { Outlet, useNavigate } from 'react-router';
 
 import { Header } from '@/components/shared';
+import { api } from '@/lib/axios';
 
 export const AppLayout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error) && error.response?.status === 401) {
+          navigate('/sign-in', { replace: true });
+        }
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <main className='flex min-h-screen flex-col antialiased'>
       <Header />
