ea653fa0a7b9198f832986abc8fa767eb38aab4d
Evandro Calado <evandrocalado07@gmail.com>
2025-03-11 20:12:51 -0300

    feat: migrate Prettier configuration from .prettierrc.json to prettier.config.cjs

---
+++ prettier.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: [
+    require.resolve('@ianvs/prettier-plugin-sort-imports'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+  importOrder: [
+    '(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
