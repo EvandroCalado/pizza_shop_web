3bece83fae873038b0bfc50ec61ae0d71839ecf7
Evandro Calado <evandrocalado07@gmail.com>
2025-03-15 14:37:56 -0300

    test: add profile update test for handling invalid data submission

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -25,3 +25,25 @@ test('update profile successfully', async ({ page }) => {
     page.getByRole('button', { name: 'Pizza Shop test' }),
   ).toBeVisible();
 });
+
+test('update profile with invalid data', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click();
+
+  await page.getByText('Perfil').click();
+
+  await page.getByRole('textbox', { name: 'Nome' }).fill('Invalid name');
+
+  await page
+    .getByRole('textbox', { name: 'Descrição' })
+    .fill('Another description');
+
+  await page.getByRole('button', { name: 'Salvar' }).click();
+
+  await page.waitForLoadState('networkidle');
+
+  await expect(
+    page.getByText('Algo deu errado, tente novamente.'),
+  ).toBeVisible();
+});
